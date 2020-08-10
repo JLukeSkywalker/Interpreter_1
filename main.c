73ef573a9ee388078c64d08dc72ed6da53dc0fb2
John Lukowski <lukowskijohn@gmail.com>
2020-08-10 16:32:17 +0000

    Added comments to entire file

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ char* readFile(char* fileName);
 int runCode(char* codeIn);
 
 // Global variables
-struct hashNode* top = NULL;
+struct hashNode* scopeStack = NULL;
 
 int main(int numArgs, char* args[]) {
     // Make sure only a fileName was given from command line
@@ -25,7 +25,7 @@ int main(int numArgs, char* args[]) {
 
     int codeRan = runCode(codeIn);
     if(codeRan != 0)  return codeRan;
-}
+}// END FUNCTION main
 
 char* readFile(char* fileName){
     FILE *fileData;
@@ -51,6 +51,7 @@ char* readFile(char* fileName){
     fclose(fileData);
 
     // Remove all spaces from the file
+    // LATER, PROBABLY EXTRACT FUNCTION POSITIONS HERE TOO
     int spaces = 0;
     for(int i=0;i<byteSize;i++){
         if(codeIn[i] != ' '){
@@ -60,19 +61,24 @@ char* readFile(char* fileName){
     codeIn[spaces] = '\0';
 
     return codeIn;
-}
+}// END FUNCTION readFile
 
 int runCode(char* codeIn){
+    // Create hashmap to store all the variables in this scope
     const unsigned initialSize = 16;
     struct hashmap variables;
     hashmap_create(initialSize, &variables);
-    push(&top, variables);
+    // Push it to the top of the scope stack
+    push(&scopeStack, variables);
 
+    // Copy over the inputted code into new array to not mutate the old one
     char runningCode[strlen(codeIn)];
     strcpy(runningCode,codeIn);
 
+    // Loop through each line of the new changeable array
     int lineNum = 0;
     int charactersRead = 0;
+    // Use strtok as an iterator for each line
     char *line = strtok(runningCode, "\n");
     while(line != NULL){
         printf("Line %d: %s\n",++lineNum,line);
@@ -84,18 +90,19 @@ int runCode(char* codeIn){
         printf("%s\n",command);
         printf("%s\n",params);
 
+        // Move iterator to next line
         line=strtok(NULL,"\n");
-    }
+    }// END WHILE
     return 0;
-}
+}// END FUNCTION runCode
 
 void mapExample(){
 
     char* text = "Hello !";
     struct Data test = {"str",text};
 
-    hashmap_put(&(*top).data, "greeting", strlen("greeting"), test);
+    hashmap_put(&(*scopeStack).data, "greeting", strlen("greeting"), test);
 
-    printf("%s, %s\n",hashmap_get(&(*top).data, "greeting", strlen("greeting")).type,(char*)hashmap_get(&(*top).data, "greeting", strlen("greeting")).data);
+    printf("%s, %s\n",hashmap_get(&(*scopeStack).data, "greeting", strlen("greeting")).type,(char*)hashmap_get(&(*scopeStack).data, "greeting", strlen("greeting")).data);
 
 }
